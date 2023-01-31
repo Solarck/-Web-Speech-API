0e6bbec0c06e5acd5cc3906336c93bb7bb3291bf
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-01-31 19:26:11 -0300

    up

---
+++ js/sortearnumero.js
@@ -0,0 +1,15 @@
+const menorValor = 1
+const maiorValor = 1000
+const numeroSecreto = NumeroAleatorio()
+
+function NumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log(numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
