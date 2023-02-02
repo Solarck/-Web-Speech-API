e4130d44e05a9ba7352eb9828fabc04f3179e52b
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-02-02 09:30:45 -0300

    .

--- js/validacao.js
+++ js/validacao.js
@@ -1,7 +1,8 @@
-function verificaChuteValidow(){
+function verificaChuteValido(){
     const numero =+ chute
 
     if(Number.isNaN(numero)){
+        const elementoChute = document.getElementById('chute')
         elementoChute.innerHTML= 'Valor invalido'
     }
 }
