974b63d4937335796ecdce81479e4777abee943d
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-02-06 18:55:27 -0300

    up

--- js/validacao.js
+++ js/validacao.js
@@ -17,15 +17,19 @@ function verificaChuteValido(chute) {
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
         `
     }
 
     else if (numero > numeroSecreto) {
-        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-up-long"></i></div> -->`
-    }
-
-    else {
-        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div> -->`
+        elementoChute.innerHTML += `
+    <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+    `
+    } else {
+        elementoChute.innerHTML += `
+    <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+    `
     }
 
     function chuteForInvalido(numero) {
@@ -36,3 +40,9 @@ function verificaChuteValido(chute) {
         return numero > maiorValor || numero < menorValor
     }
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
